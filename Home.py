f285e2ad84ed4733d75c19255220bd02f22208c4
Jingwei Chen <030215czcjw@gmail.com>
2025-12-25 17:01:12 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -27,7 +27,7 @@ st.markdown("""
     注意事项：
     - 表格
         - 表格名称为数据名称
-        - 表格内第一列放日期，第二列放原始值（如果加载错误试试把第一列列名改为 日期、Date或time）
+        - 表格内第一列放日期，第二列放原始值(如果加载错误试试把第一列列名改为 日期、Date或time)
         - 煤炭 https://docs.google.com/spreadsheets/d/1P3446_9mBi-7qrAMi78F1gHDHGIOCjw-/edit?usp=drive_link&ouid=116487443839473589964&rtpof=true&sd=true 在这更新！
         - 交运 https://docs.google.com/spreadsheets/d/1VVTAG1ixDe50ysjMZEAAZyvYkUbiHBvolh0oaYn8Mxw/edit?usp=drive_link 在这更新！
     - 数据日期是取特征、标的、基准的交集
@@ -38,5 +38,5 @@ st.markdown("""
 st.divider()
 
 st.markdown("""
-    - 源码：https://github.com/030215czcjw-pixel/Industry_Quant_System
+    - 源码:https://github.com/030215czcjw-pixel/Industry_Quant_System
 """)
