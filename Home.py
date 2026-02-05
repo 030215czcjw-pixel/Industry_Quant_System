de808f985eaa7c56e107ca589ba7de9de78f23be
“030215czcjw-pixel“ <030215czcjw@gmail.com>
2026-02-05 14:33:38 +0800

    add coal

--- Home.py
+++ Home.py
@@ -34,6 +34,7 @@ st.markdown("""
         - 煤炭 https://docs.google.com/spreadsheets/d/1P3446_9mBi-7qrAMi78F1gHDHGIOCjw-/edit?usp=drive_link&ouid=116487443839473589964&rtpof=true&sd=true 在这更新！
         - 交运 https://docs.google.com/spreadsheets/d/1VVTAG1ixDe50ysjMZEAAZyvYkUbiHBvolh0oaYn8Mxw/edit?usp=drive_link 在这更新！
         - 游戏 https://docs.google.com/spreadsheets/d/1jV7SLw7fOrKmH8SkGYOMtYZL-4ig1E-DxK77jb32N5Y/edit?usp=sharing
+        - 焦煤 https://docs.google.com/spreadsheets/d/1_ye6vJegv8KMzDQLjaBDlf-GDkD7s9dfr3p2Yyr0dNw/edit?usp=sharing
         - 上面的要挂🪜 
     - 数据日期是取特征、标的、基准的交集
     - 选取卡尔曼滤波之后，后续计算均基于滤波后的数据
